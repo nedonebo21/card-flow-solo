538413847f5e8f559daa4df20fdf4a8f2ab3f73f
nedonebo21 <cursed.void10007@gmail.com>
2025-10-11 15:57:51 +0400

    feat: add icons + iconography

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,8 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-   addons: [],
+   stories: ['../.storybook/stories/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+   addons: ['@storybook/addon-docs'],
    framework: {
       name: '@storybook/react-vite',
       options: {},
