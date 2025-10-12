12911e69d137679383e6af764c38c6f2eb8d99bf
nedonebo21 <cursed.void10007@gmail.com>
2025-10-12 16:25:35 +0400

    feat: add checkbox

---
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -0,0 +1,40 @@
+import type { CheckboxProps as RadixCheckboxProps } from '@radix-ui/react-checkbox'
+import type { ReactNode } from 'react'
+
+import { Checkbox as RadixCheckbox } from '@radix-ui/react-checkbox'
+
+import { clsx } from 'clsx'
+
+import { CheckIcon } from '@/shared/ui/icons'
+import { Typography } from '@/shared/ui/typography'
+
+import styles from './checkbox.module.scss'
+
+export type CheckboxProps = {
+   children?: ReactNode
+   label?: string
+} & RadixCheckboxProps
+
+export const Checkbox = ({ className, label, children, checked, ...rest }: CheckboxProps) => {
+   return (
+      <label className={styles.checkboxWrapper}>
+         <RadixCheckbox {...rest} className={clsx(styles.checkbox, className)}>
+            <span className={styles.checkboxIndicator}>
+               {checked && (
+                  <CheckIcon width={14} height={11} className={styles.checkboxIndicator} />
+               )}
+            </span>
+         </RadixCheckbox>
+         {(label || children) && (
+            <Typography
+               variant={'body2'}
+               as={'span'}
+               className={styles.checkboxLabel}
+               aria-disabled={!!rest.disabled}
+            >
+               {label || children}
+            </Typography>
+         )}
+      </label>
+   )
+}
