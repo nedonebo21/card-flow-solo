c85299228f2e1481457da15ae9eab0cd67e8470f
nedonebo21 <cursed.void10007@gmail.com>
2025-10-18 14:43:08 +0400

    refactor: typography types, props, classes

--- src/shared/ui/typography/typography.tsx
+++ src/shared/ui/typography/typography.tsx
@@ -15,53 +15,48 @@ const variantMapping = {
    subtitle2: 'p',
    caption: 'span',
    overline: 'span',
-   link1: 'a',
-   link2: 'a',
+   link1: 'span',
+   link2: 'span',
    error: 'span',
    warning: 'span',
 } as const satisfies Record<string, ElementType>
 
 export type TypographyVariant = keyof typeof variantMapping
-export type TypographyWeight = 'light' | 'regular' | 'medium' | 'bold'
-export type TypographyColor = 'primary' | 'secondary' | 'inherit'
 
 export type TypographyOwnProps<T extends ElementType = ElementType> = {
    as?: T
    children: ReactNode
    variant?: TypographyVariant
-   weight?: TypographyWeight
-   color?: TypographyColor
-} & ComponentProps<'span'>
+   textAlign?: 'center' | 'justify' | 'left' | 'right'
+   noWrap?: boolean
+}
 
 export type TypographyProps<T extends ElementType> = TypographyOwnProps<T> &
    Omit<ComponentProps<T>, keyof TypographyOwnProps>
 
-const classes = ({
-   variant = 'body1',
-   weight = 'regular',
-   color = 'primary',
-   className,
-}: {
-   variant?: TypographyVariant
-   weight?: TypographyWeight
-   color?: TypographyColor
-   className?: string
-}) => clsx(styles.button, styles[variant], styles[weight], styles[color], className)
-
-export const Typography = <T extends ElementType>({
+export const Typography = <T extends ElementType = 'p'>({
    as,
    variant = 'body1',
-   weight = 'regular',
-   color = 'inherit',
-   children,
+   textAlign = 'center',
+   noWrap,
    className,
    ...rest
 }: TypographyProps<T>) => {
-   const Component = as || variantMapping[variant]
-
-   return (
-      <Component {...rest} className={classes({ variant, weight, color, className })}>
-         {children}
-      </Component>
+   const classes = clsx(
+      styles[variant],
+      {
+         'no-wrap': noWrap,
+         'align-center': textAlign === 'center',
+         'align-right': textAlign === 'right',
+         'align-left': textAlign === 'left',
+         'align-justify': textAlign === 'justify',
+      },
+      className
    )
+
+   const componentTag = variantMapping[variant] ?? 'p'
+
+   const Component = as ?? componentTag
+
+   return <Component className={classes} {...rest} />
 }
