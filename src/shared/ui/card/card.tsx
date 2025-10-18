0a40bc10d9c268706e894767e7e9a746428918fe
nedonebo21 <cursed.void10007@gmail.com>
2025-10-18 10:54:43 +0400

    refactor: props, children

--- src/shared/ui/card/card.tsx
+++ src/shared/ui/card/card.tsx
@@ -1,4 +1,4 @@
-import type { ComponentProps, ElementType, ReactNode } from 'react'
+import type { ComponentProps, ElementType } from 'react'
 
 import { clsx } from 'clsx'
 
@@ -7,23 +7,13 @@ import styles from './card.module.scss'
 export type CardOwnProps<T extends ElementType = ElementType> = {
    as?: T
    className?: string
-   children?: ReactNode
-} & ComponentProps<'div'>
+}
 
 export type CardProps<T extends ElementType = ElementType> = CardOwnProps<T> &
    Omit<ComponentProps<T>, keyof CardOwnProps>
 
-export const Card = <T extends ElementType = 'div'>({
-   as,
-   className,
-   children,
-   ...rest
-}: CardProps<T>) => {
-   const Component = as || 'div'
+export const Card = <T extends ElementType = 'div'>({ as, className, ...rest }: CardProps<T>) => {
+   const Component = as ?? 'div'
 
-   return (
-      <Component className={clsx(styles.card, className)} {...rest}>
-         {children}
-      </Component>
-   )
+   return <Component className={clsx(styles.card, className)} {...rest} />
 }
