84085f3414e6a20e3aa93b5737a47c9bee6d45f2
nedonebo21 <cursed.void10007@gmail.com>
2025-11-09 16:49:33 +0400

    refactor: remake dropdown

--- src/shared/ui/dropdown/dropdown.stories.tsx
+++ src/shared/ui/dropdown/dropdown.stories.tsx
@@ -7,7 +7,6 @@ import { Dropdown } from '@/shared/ui/dropdown/dropdown'
 import {
    CirclePlayIcon,
    LogOutIcon,
-   MoreVerticalIcon,
    PencilIcon,
    TrashIcon,
    UserFilledIcon,
@@ -34,31 +33,24 @@ export const Default: Story = {
 
       return (
          <Dropdown open={isOpen} onOpenChange={setIsOpen}>
-            <Dropdown.Trigger>
-               <Button variant={'ghost'} size={'icon'}>
-                  <MoreVerticalIcon width={24} height={24} />
+            <Dropdown.Item>
+               <Button variant={'ghost'}>
+                  <CirclePlayIcon width={16} height={16} />
+                  <Typography variant={'caption'}>Learn</Typography>
                </Button>
-            </Dropdown.Trigger>
-            <Dropdown.Content>
-               <Dropdown.Item>
-                  <Button variant={'ghost'}>
-                     <CirclePlayIcon width={16} height={16} />
-                     <Typography variant={'caption'}>Learn</Typography>
-                  </Button>
-               </Dropdown.Item>
-               <Dropdown.Item>
-                  <Button variant={'ghost'}>
-                     <PencilIcon width={16} height={16} />
-                     <Typography variant={'caption'}>Edit</Typography>
-                  </Button>
-               </Dropdown.Item>
-               <Dropdown.Item>
-                  <Button variant={'ghost'}>
-                     <TrashIcon width={16} height={16} />
-                     <Typography variant={'caption'}>Delete</Typography>
-                  </Button>
-               </Dropdown.Item>
-            </Dropdown.Content>
+            </Dropdown.Item>
+            <Dropdown.Item>
+               <Button variant={'ghost'}>
+                  <PencilIcon width={16} height={16} />
+                  <Typography variant={'caption'}>Edit</Typography>
+               </Button>
+            </Dropdown.Item>
+            <Dropdown.Item>
+               <Button variant={'ghost'}>
+                  <TrashIcon width={16} height={16} />
+                  <Typography variant={'caption'}>Delete</Typography>
+               </Button>
+            </Dropdown.Item>
          </Dropdown>
       )
    },
@@ -70,35 +62,38 @@ export const ProfileDemo: Story = {
       const [isOpen, setIsOpen] = useState(false)
 
       return (
-         <Dropdown open={isOpen} onOpenChange={setIsOpen}>
-            <Dropdown.Trigger>
-               <TrashIcon width={36} height={36} />
-            </Dropdown.Trigger>
-            <Dropdown.Content>
-               <Dropdown.Label>
+         <Dropdown
+            open={isOpen}
+            onOpenChange={setIsOpen}
+            trigger={
+               <Button variant={'ghost'} size={'icon'}>
                   <TrashIcon width={36} height={36} />
-                  <div className={styles.info}>
-                     <Typography variant={'subtitle2'} as={'span'} textAlign={'left'}>
-                        Ivan
-                     </Typography>
-                     <Typography variant={'caption'} className={styles.email} textAlign={'left'}>
-                        example@example.com
-                     </Typography>
-                  </div>
-               </Dropdown.Label>
-               <Dropdown.Item>
-                  <Button variant={'ghost'}>
-                     <UserFilledIcon width={16} height={16} />
-                     <Typography variant={'caption'}>My Profile</Typography>
-                  </Button>
-               </Dropdown.Item>
-               <Dropdown.Item>
-                  <Button variant={'ghost'}>
-                     <LogOutIcon width={16} height={16} />
-                     <Typography variant={'caption'}>Sign Out</Typography>
-                  </Button>
-               </Dropdown.Item>
-            </Dropdown.Content>
+               </Button>
+            }
+         >
+            <Dropdown.Label>
+               <TrashIcon width={36} height={36} />
+               <div className={styles.info}>
+                  <Typography variant={'subtitle2'} as={'span'} textAlign={'left'}>
+                     Ivan
+                  </Typography>
+                  <Typography variant={'caption'} className={styles.email} textAlign={'left'}>
+                     example@example.com
+                  </Typography>
+               </div>
+            </Dropdown.Label>
+            <Dropdown.Item>
+               <Button variant={'ghost'}>
+                  <UserFilledIcon width={16} height={16} />
+                  <Typography variant={'caption'}>My Profile</Typography>
+               </Button>
+            </Dropdown.Item>
+            <Dropdown.Item>
+               <Button variant={'ghost'}>
+                  <LogOutIcon width={16} height={16} />
+                  <Typography variant={'caption'}>Sign Out</Typography>
+               </Button>
+            </Dropdown.Item>
          </Dropdown>
       )
    },
--- src/shared/ui/dropdown/dropdown.tsx
+++ src/shared/ui/dropdown/dropdown.tsx
@@ -4,69 +4,55 @@ import * as RadixDropdown from '@radix-ui/react-dropdown-menu'
 
 import { clsx } from 'clsx'
 
+import { Button } from '@/shared/ui/button'
 import { Card } from '@/shared/ui/card'
+import { MoreVerticalIcon } from '@/shared/ui/icons'
 
 import styles from './dropdown.module.scss'
 
 type DropdownProps = {
-   children: ReactNode
-} & Omit<ComponentProps<typeof RadixDropdown.Root>, 'asChild' | 'children'>
+   trigger?: ReactNode
+} & Omit<ComponentProps<typeof RadixDropdown.Root>, 'asChild'>
 
-const Dropdown = ({ ...rest }: DropdownProps) => {
-   return <RadixDropdown.Root {...rest} />
-}
-
-type DropdownTriggerProps = {
-   children: ReactNode
-} & Omit<ComponentProps<typeof RadixDropdown.Trigger>, 'children' | 'asChild'>
-
-export const DropdownTrigger = ({ className, ...rest }: DropdownTriggerProps) => {
+const Dropdown = ({
+   children,
+   trigger = (
+      <Button variant={'ghost'} size={'icon'}>
+         <MoreVerticalIcon width={24} height={24} />
+      </Button>
+   ),
+   ...rest
+}: DropdownProps) => {
    return (
-      <RadixDropdown.Trigger
-         className={clsx(styles.trigger, className)}
-         {...rest}
-         asChild={undefined}
-      />
+      <RadixDropdown.Root {...rest}>
+         <RadixDropdown.Trigger className={styles.trigger}>{trigger}</RadixDropdown.Trigger>
+         <RadixDropdown.Portal>
+            <RadixDropdown.Content
+               sideOffset={1}
+               align={'end'}
+               className={styles.container}
+               {...rest}
+            >
+               <Card className={styles.content}>{children}</Card>
+               <RadixDropdown.Arrow className={styles.arrow} />
+            </RadixDropdown.Content>
+         </RadixDropdown.Portal>
+      </RadixDropdown.Root>
    )
 }
 
-type DropdownContentProps = {
-   children: ReactNode
-} & Omit<ComponentProps<typeof RadixDropdown.Content>, 'asChild' | 'children'>
-
-export const DropdownContent = ({ className, children, ...rest }: DropdownContentProps) => {
-   return (
-      <RadixDropdown.Portal>
-         <RadixDropdown.Content sideOffset={1} align={'end'} className={styles.container} {...rest}>
-            <Card className={clsx(styles.content, className)}>{children}</Card>
-            <RadixDropdown.Arrow className={styles.arrow} />
-         </RadixDropdown.Content>
-      </RadixDropdown.Portal>
-   )
-}
-
-type DropdownLabelProps = {
-   children: ReactNode
-} & Omit<ComponentProps<typeof RadixDropdown.Label>, 'asChild' | 'children'>
+type DropdownLabelProps = Omit<ComponentProps<typeof RadixDropdown.Label>, 'asChild'>
 
 const DropdownLabel = ({ className, ...rest }: DropdownLabelProps) => {
    return <RadixDropdown.Label className={clsx(styles.label)} {...rest} />
 }
 
-type DropdownItemProps = {
-   children: ReactNode
-} & Omit<ComponentProps<typeof RadixDropdown.Item>, 'asChild' | 'children'>
+type DropdownItemProps = Omit<ComponentProps<typeof RadixDropdown.Item>, 'asChild'>
 
-const DropdownItem = ({ className, children, ...rest }: DropdownItemProps) => {
-   return (
-      <RadixDropdown.Item className={clsx(styles.item, className)} {...rest}>
-         {children}
-      </RadixDropdown.Item>
-   )
+const DropdownItem = ({ className, ...rest }: DropdownItemProps) => {
+   return <RadixDropdown.Item className={clsx(styles.item, className)} {...rest} />
 }
 
-Dropdown.Trigger = DropdownTrigger
-Dropdown.Content = DropdownContent
 Dropdown.Label = DropdownLabel
 Dropdown.Item = DropdownItem
 
