9faaed49af2376bd769c580f286a8a865d2930ea
nedonebo21 <cursed.void10007@gmail.com>
2025-11-11 14:29:17 +0400

    refactor: add custom errors in sign-up-schema

--- src/features/auth/model/sign-up-schema.ts
+++ src/features/auth/model/sign-up-schema.ts
@@ -2,12 +2,15 @@ import { z } from 'zod'
 
 export const signUpSchema = z
    .object({
-      email: z.email(),
-      password: z.string().min(3),
+      email: z.email('Enter a valid email address').transform(email => email.trim()),
+      password: z
+         .string()
+         .min(3, 'Password must be longer than 3 characters')
+         .refine(pass => !pass.includes(' '), 'Password cannot contains space`s'),
       confirmedPassword: z.string().min(3),
    })
    .refine(data => data.confirmedPassword === data.password, {
-      message: 'Passwords don`t match',
+      message: 'Passwords must be match',
       path: ['confirmedPassword'],
    })
 
