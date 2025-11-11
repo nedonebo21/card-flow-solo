cd8d9a0253eb773b6df629280da5fc69e564b589
nedonebo21 <cursed.void10007@gmail.com>
2025-11-11 14:23:39 +0400

    refactor: add custom errors in login-schema

--- src/features/auth/model/login-schema.ts
+++ src/features/auth/model/login-schema.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod'
 
 export const loginSchema = z.object({
-   email: z.email(),
-   password: z.string().min(3),
+   email: z.email('Enter a valid email address').transform(email => email.trim()),
+   password: z
+      .string()
+      .min(3, 'Password must be longer than 3 characters')
+      .refine(pass => !pass.includes(' '), 'Password cannot contains space`s'),
    rememberMe: z.boolean().optional(),
 })
 
