781561895dec33a4ee269a9c736c45f7df74f2fc
nedonebo21 <cursed.void10007@gmail.com>
2025-11-22 18:07:46 +0400

    refactor: remove redundant condition

--- src/app/routing/private-routes.tsx
+++ src/app/routing/private-routes.tsx
@@ -1,20 +1,15 @@
-import { Navigate, Outlet, useNavigate } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 
 import { useMeQuery } from '@/entities/user/api/user-api'
 
 export const PrivateRoutes = () => {
    const { isLoading, isError } = useMeQuery()
-   const navigate = useNavigate()
 
    if (isLoading) {
       return <div>Loading...</div>
    }
 
-   if (isError) {
-      navigate('/sign-in')
-   }
-
    const isAuth = !isError
 
-   return isAuth ? <Outlet /> : <Navigate to={'/sign-in'} />
+   return isAuth ? <Outlet /> : <Navigate to={'/sign-in'} replace />
 }
