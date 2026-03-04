15747fb8b7e8ca4c522673cf5df9127c419c2635
nedonebo21 <cursed.void10007@gmail.com>
2026-03-04 11:24:50 +0300

    feat: added i18next-http-backend

--- src/shared/lib/i18n/i18n.ts
+++ src/shared/lib/i18n/i18n.ts
@@ -2,6 +2,7 @@ import { initReactI18next } from 'react-i18next'
 
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import HttpBackend from 'i18next-http-backend'
 
 import enTranslations from '@/shared/lib/i18n/locales/en.json'
 import ruTranslations from '@/shared/lib/i18n/locales/ru.json'
@@ -11,8 +12,8 @@ export const resources = {
    ru: { translation: ruTranslations },
 }
 
-i18n.use(LanguageDetector).use(initReactI18next).init({
-   debug: true,
+i18n.use(HttpBackend).use(LanguageDetector).use(initReactI18next).init({
+   debug: false,
    fallbackLng: 'en',
    resources,
 })
